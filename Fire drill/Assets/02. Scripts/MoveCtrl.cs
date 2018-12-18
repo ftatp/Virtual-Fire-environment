072bd9fd557fc381a46900cb638f3324fba2a0f3
ftatp <ftatp5901@gmail.com>
2018-12-18 11:27:32 +0900

    first code attention

---
+++ Fire drill/Assets/02. Scripts/MoveCtrl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour {
+
+	public enum MoveType{
+		WAY_POINT,
+		LOOK_AT,
+		DAYDREAM
+	}
+
+	public MoveType movetype = MoveType.LOOK_AT;
+	public float moveSpeed = 2.0f;
+	public float rotSpeed = 40.0f;
+	private float damping = 3.0f;
+
+	private Transform tr;
+	private Transform camTr;
+	private CharacterController cc;
+
+	private Transform[] points;
+	private int nextIdx = 1;
+
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent<Transform>();
+		camTr = Camera.main.GetComponent<Transform>();
+		cc = GetComponent<CharacterController>();
+		//points = GameObject.Find("WayPointGroup").GetComponentsInChildren<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(movetype){
+			case MoveType.WAY_POINT:
+				MoveWayPoint();
+				break;
+			case MoveType.LOOK_AT:
+				MoveLookAt();
+				break;
+			case MoveType.DAYDREAM:
+				break;
+		}
+	}
+
+	void MoveWayPoint(){
+		Vector3 direction = points[nextIdx].position - tr.position;
+
+		Quaternion rot = Quaternion.LookRotation(direction);
+		//tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * damping);
+
+		tr.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+	}
+
+	void OnTriggerEnter(Collider coll){
+		if(coll.CompareTag("WAY_POINT")){
+			//Debug.Log("!@#$%");
+			nextIdx = (++nextIdx >= points.Length) ? 1 : nextIdx;
+		}
+	}
+
+	void MoveLookAt(){
+		Vector3 dir = camTr.TransformDirection(Vector3.forward);
+		//tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+		cc.SimpleMove(dir * moveSpeed);
+	}
+
+}
---
+++ Fire drill/Assets/02. Scripts/PlayerCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+	private float h = 0.0f;
+	private float v = 0.0f;
+
+	private Transform tr;
+	public float movespeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+
+		//Debug.Log("h = " + h.ToString());
+		//Debug.Log("v = " + v.ToString());
+
+		Vector3 moveDir = Vector3.forward * v + Vector3.right * h;
+		tr.Translate(moveDir.normalized * movespeed * Time.deltaTime, Space.Self);
+	}
+}
